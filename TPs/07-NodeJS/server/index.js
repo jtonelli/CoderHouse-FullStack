b921de3e1506a6188d19c23d6062326ea23155c0
Juan Tonelli <j.tonelli90@gmail.com>
2015-09-11 12:43:16 -0300

    Consume Meli API with Request

--- TPs/07-NodeJS/server/index.js
+++ TPs/07-NodeJS/server/index.js
@@ -1,13 +1,43 @@
 var rta = require('./respuesta');
 
+var request = require('request');
+
 var https = require("https");
 
 var express = require('express');
 var app = express();
+var appReq = express();
+
+appReq.get('/meliProxy', function (req, res) {
+  // console.log(req.query.q)
+  var url = 'https://api.mercadolibre.com/sites/MLA/search?q=' + req.query.q + '&limit=1';
+
+  request.get(url, function (err, resMeli, body) {
+      if (!err) {
+          // var resultsObj = JSON.parse(body);
+          //Just an example of how to access properties:
+          res.json(body.toString());
+      }
+  });
+
+  // https.get(url, function(resMeli) {
+  //     // console.log("Got response: " + res.statusCode);
+  //     resMeli.on("data", function(d) {
+  //     // res.json(d.toString());
+  //      body += d;
+  //   });
+  //   resMeli.on('end', function() {
+  //       res.json(body.toString());
+  //     });
+  // }).on('error', function(e) {
+  //     console.log("Got error: " + e.message);
+  // });
+
+});
 
 app.get('/meliProxy', function (req, res) {
   // console.log(req.query.q)
-	var url = 'https://api.mercadolibre.com/sites/MLA/search?q=' + req.query.q + '&limit=3';
+	var url = 'https://api.mercadolibre.com/sites/MLA/search?q=' + req.query.q + '&limit=1';
 	var body = '';
 
 	https.get(url, function(resMeli) {
@@ -46,4 +76,8 @@ app.get('/cuatro', function (req, res) {
 
 var server = app.listen(8045, function () {
   // Escuchar puerto 8045
+});
+
+var serverReq = appReq.listen(8054, function () {
+  // Escuchar puerto 8054
 });
