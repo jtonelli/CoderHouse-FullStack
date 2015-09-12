1664e2a78d3902ab74440abcc3cc65eff5dbf3d0
Juan Tonelli <j.tonelli90@gmail.com>
2015-09-12 12:07:37 -0300

    Clase 8 - MongoBD + Mongoose

--- Clases/8_Node/server/index.js
+++ Clases/8_Node/server/index.js
@@ -1,25 +1,68 @@
-var mod = require('./myModulo');
-var om = require('./OtroModulo');
+var mdb = require('mongoose');
+var urldb = 'mongodb://admin:admin@ds041581.mongolab.com:41581/tpairbnb';
 
-var fs = require('fs');
-var ld = require('lodash');
+//Establecer conexion.
+mdb.connect(urldb, function(err){
+	if(err){
+		console.log('DB connect - Error: ', err);
+	}
+	else{
+		console.log('Se conecto OK');
+	}
+})
 
-var saludo = 'Hola';
+//Schema = Cuerpo del modelo.
+var schema = new mdb.Schema({ 
+		name: 'string', 
+		edad: Number
+	});
 
-var saludar = function() {
-	return 'Hola mundo';
-};
+//El modelo - Entidad
+var User = mdb.model('User', schema);
 
-fs.writeFile('message.txt', 'Hello Node',
-	function(err){
-		if(!err){
-			console.log('Se guardo!!');
-		}else{
-			console.log('Error');
-		}
+////Creo un usuario
+// var u = new User({ name: 'juan' });
+
+////Grabo el usuario
+// u.save(function (err) {
+//   // if (err) return handleError(err);
+//   if(err){
+//   	console.log('Error - ', err);
+//   }
+//   else{
+//   	console.log(u);
+//   	u.name = 'JuaniTone';
+//   	u.save();
+//   }
+//   // saved!
+// });
+
+////Actualizar Datos de toda la tabla.
+// var query = {
+// 	edad:{
+// 		$lt:50
+// 	}
+// };
+var query = {};
+var update = {edad : 25};
+var options = {multi: true};
+User.update(query, update, options, function(err, docs){
+	console.log(docs);
 });
 
+// //Busqueda sin parametros con modificacion de datos
+// User.find({}, function(err, docs){
+// 	var primero = docs[0];
+	
+// 	primero.name = 'Juani';
 
-console.log(ld.now());
-console.log(mod);
-console.log(om);
+// 	primero.save(function(err){
+// 		console.log('Usuario', primero)
+// 	});
+
+// });
+
+// // Busqueda con filtro
+// User.find({name : 'JuaniTone'}, function(err, docs){
+// 	console.log('Usuarios', docs);	
+// });
